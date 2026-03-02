9f1949dd28791230abf5efc75a7a6d026968239f
AmsSaikat <amssaikat@gmail.com>
2026-03-03 00:56:56 +0600

    Fixed Socket io issues

--- backEnd/config/socketConn.js
+++ backEnd/config/socketConn.js
@@ -1,9 +1,15 @@
 import { Server } from "socket.io";
 
 const setupChatSocket = (server) => {
+
+  const allowedOrigins = [
+  process.env.CLIENT_URL,
+  process.env.NETLIFY_URL
+];
+
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: allowedOrigins,
       methods: ["GET", "POST"]
     }
   });
